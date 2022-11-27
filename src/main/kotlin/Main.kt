9d7826206d2368e0a1d908c484b50f792bf57fcc
jonathanbasuki <basukijonathan4@gmail.com>
2022-11-27 19:37:44 +0700

    added login authentication

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,9 +1,45 @@
-import dataMahasiswa.mahasiswa1
+import dataMahasiswa.dataMahasiswa
+import java.util.Scanner
 
 fun main() {
-    println("Hello World!")
-    println("GILS BWANG")
-    println("ASIK BAT LU BWANG")
-    println("CHILL PARAH")
-    println("LU PRO BANG")
+    // buat variabel utk validasi login
+    var dataExist: Boolean = false
+    var password: String? = null
+
+    // akan melakukan looping jika nim tidak ditemukan || password salah
+    do {
+        // ambil input dari user
+        print("Masukkan NIM: ")
+        var inputNim = readLine()?.toLong() // -> konversi tipe data
+
+        print("Masukkan Password: ")
+        var inputPassword = readLine()
+
+        // akan melakukan looping utk ngecek data mahasiswa yg ada
+        for (mahasiswa in dataMahasiswa) {
+            // akan melakukan validasi password berdasarkan nim yg dimasukkan
+            if (mahasiswa.nim == inputNim) {
+                dataExist = true
+                password = mahasiswa.password
+
+                // inisialisasi variabel data mahasiswa
+                val nim = mahasiswa.nim
+                val nama = mahasiswa.nama
+                val email = mahasiswa.email
+                val prodi = mahasiswa.prodi
+
+                // akan melakukan validasi password
+                if (password == inputPassword) {
+                    println("Berhasil login!")
+                } else {
+                    dataExist = false
+                }
+            }
+        }
+
+        // akan menampilkan pesan jika nim tidak ditemukan || password salah
+        if (!dataExist) {
+            println("NIM atau Password salah!")
+        }
+    } while (!dataExist || password != inputPassword)
 }
--- src/main/kotlin/dataMahasiswa/Mahasiswa.kt
+++ src/main/kotlin/dataMahasiswa/Mahasiswa.kt
@@ -5,8 +5,13 @@ data class Mahasiswa(
     val nama: String,
     val email: String,
     val password: String,
-    val prodi: String,
+    val prodi: String
 )
 
-val mahasiswa1 = Mahasiswa(2210511007, "Muhammad Kahfi Darmawan", "2210511007@mahasiswa.upnvj.ac.id", "kahfi", "Informatika")
+// instansiasi data class mahasiswa
+val mahasiswa1 = Mahasiswa(2210511007,"Muhammad Kahfi Darmawan", "2210511007@mahasiswa.upnvj.ac.id", "kahfi", "Informatika")
 val mahasiswa2 = Mahasiswa(2210511015, "Jonathan Christoper Basuki", "2210511015@mahasiswa.upnvj.ac.id", "jonathan", "Informatika")
+
+// membuat list data mahasiswa utk keperluan validasi login
+var dataMahasiswa = listOf<Mahasiswa>(mahasiswa1, mahasiswa2)
+
