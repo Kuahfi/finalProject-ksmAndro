ccbaa9a5d61c2c84cf9cd390425b771215a33070
jonathanbasuki <basukijonathan4@gmail.com>
2022-12-11 16:05:36 +0700

    added exception handling (try catch)

--- src/main/kotlin/dataMahasiswa/InterfaceMahasiswa.kt
+++ src/main/kotlin/dataMahasiswa/InterfaceMahasiswa.kt
@@ -1,12 +1,20 @@
 package dataMahasiswa
 
+// deklarasi interface
 interface InterfaceMahasiswa {
     val nim: Long
     val nama: String
-    val email: String
-    val password: String
     val prodi: String
 
+    // buat fungsi absen
     fun absen(): Unit {
+        println("""
+================================================================================            
+|            $nama Silahkan isi absen di bawah ini:                            |
+|            [1]: Hadir                                                        |
+|            [2]: Sakit                                                        |
+|            [3]: Izin                                                         |
+================================================================================
+        """.trimIndent())
     }
 }
